5e3f435710aaf378f973d7f1c305d94e276d2790
Chandra Mouli <chandu.prince951@gmail.com>
2019-02-10 18:19:02 -0800

    Fixed toolbar extension

--- myjs.js
+++ myjs.js
@@ -1,5 +1,7 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({'url': chrome.extension.getURL('f.html')}, function(tab) {
+  chrome.tabs.create({'url': chrome.extension.getURL('newtab.html')}, function(tab) {
     // Tab opened.
   });
 });
+
+//onfocus="var temp_value=this.value; this.value=""; this.value=temp_value"
